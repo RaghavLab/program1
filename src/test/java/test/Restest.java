2b395226ba195ebcccdcefe9cfd6dbf13a66fc92
RaghavLab <rraghava1595@gmail.com>
2026-02-10 15:39:23 +0530

    Update Restest.java

--- src/test/java/test/Restest.java
+++ src/test/java/test/Restest.java
@@ -3,12 +3,15 @@ import p1.app1;
 import org.testng.*;
 
 public class Restest {
+	@Test
 	public void testPass() {
 		org.testng.Assert.assertEquals(app1.display(55),"pass");
 	}
+	@Test
 	public void testFail() {
 		org.testng.Assert.assertEquals(app1.display(39),"fail");
 	}
+	@Test
 	public void testinvalid() {
 		org.testng.Assert.assertEquals(app1.display(-1),"invalid");
 	}
